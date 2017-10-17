c9bb78593b8c153b0bf977a3462c789c7457c172
Lyoness <carmen.andoh@gmail.com>
2017-10-17 12:12:48 -0400

    Hardcodes paths for vsphere folder structure.
    Terraform does not allow us to proceed with a `terraform apply`
    Trying to hardcode these paths to proceed

--- modules/macstadium_infrastructure/folder_structure.tf
+++ modules/macstadium_infrastructure/folder_structure.tf
@@ -1,23 +1,23 @@
 resource "vsphere_folder" "base_vms" {
-  path       = "Base VMs"
+  path       = "VMs and Templates/Base VMs"
   datacenter_id = "${var.datacenter}"
   type = "vm"
 }
 
 resource "vsphere_folder" "build_vms" {
-  path       = "Build VMs"
+  path       = "VMs and Templates/Build VMs"
   datacenter_id = "${var.datacenter}"
   type = "vm"
 }
 
 resource "vsphere_folder" "internal_vms" {
-  path       = "Internal VMs"
+  path       = "VMs and Templates/Internal VMs"
   datacenter_id = "${var.datacenter}"
   type = "vm"
 }
 
 resource "vsphere_folder" "vanilla_vms" {
-  path       = "Vanilla VMs"
+  path       = "VMs and Templates/Vanilla VMs"
   datacenter_id = "${var.datacenter}"
   type = "vm"
-}
+} 
